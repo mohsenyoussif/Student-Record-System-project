0503cd114d2a5f9309dc8744e2e71355d1ee199f
mohsen youssif <mohsenyoussif2000@gmail.comgit config --global user.email mohsenyoussif2000@gmail.com>
2022-05-15 12:18:28 +0200

    adding  cmake-build-debug

---
+++ cmake-build-debug/CMakeFiles/Student_Record_System_project.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "F:/mohsen/Self Study/embedded/c programing/Student Record System project/admin_privileges.c" "F:/mohsen/Self Study/embedded/c programing/Student Record System project/cmake-build-debug/CMakeFiles/Student_Record_System_project.dir/admin_privileges.c.obj"
+  "F:/mohsen/Self Study/embedded/c programing/Student Record System project/main.c" "F:/mohsen/Self Study/embedded/c programing/Student Record System project/cmake-build-debug/CMakeFiles/Student_Record_System_project.dir/main.c.obj"
+  "F:/mohsen/Self Study/embedded/c programing/Student Record System project/system.c" "F:/mohsen/Self Study/embedded/c programing/Student Record System project/cmake-build-debug/CMakeFiles/Student_Record_System_project.dir/system.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Student_Record_System_project.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Student_Record_System_project.dir/admin_privileges.c.obj"
+  "CMakeFiles/Student_Record_System_project.dir/main.c.obj"
+  "CMakeFiles/Student_Record_System_project.dir/system.c.obj"
+  "Student_Record_System_project.exe"
+  "Student_Record_System_project.exe.manifest"
+  "Student_Record_System_project.pdb"
+  "libStudent_Record_System_project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Student_Record_System_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
